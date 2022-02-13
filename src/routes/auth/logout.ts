af29b93338f394754d2e9879730a8a2c98ada41d
cnshsliu <liukehong@gmail.com>
2022-02-13 15:50:37 +0800

    Clear filterStorage on logout

--- src/routes/auth/logout.ts
+++ src/routes/auth/logout.ts
@@ -1,6 +1,8 @@
+import { filterStorage } from '$lib/empstores';
 export function post(request) {
 	request.locals.perm = null;
 	request.locals.user = null;
+	filterStorage.set({});
 	return {
 		headers: {
 			'set-cookie': 'jwt=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'
