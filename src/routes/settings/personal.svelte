b8eb9e90994cfca3be7f2bb80dbe23a6b468ce25
cnshsliu <liukehong@gmail.com>
2022-04-08 12:10:54 +0800

    password hint I18N

--- src/routes/settings/personal.svelte
+++ src/routes/settings/personal.svelte
@@ -160,8 +160,7 @@
 						{$_('setting.set')}
 					</Button>
 				</InputGroup>
-				密码格式要求： 1. 总长度6-20之间； 2. 大小写字母、数字、特殊字符三类，每一类均至少包含一个；3.
-				特殊字符仅限!@#$%^&*
+				{$_('setting.personal.passwordhint')}
 			</Col>
 			<Col>
 				<InputGroup class="mb-1">
